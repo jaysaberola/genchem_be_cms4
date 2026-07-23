56d2aaba55e6724853b8c4b3acdd21abe914c62d
jaysaberola <wsi.jayanthonysaberola@gmail.com>
2026-07-23 11:21:34 +0800

    changes

--- public/lib/custom-grapesjs/assets/js/custom-grapesjs.js
+++ public/lib/custom-grapesjs/assets/js/custom-grapesjs.js
@@ -121,9 +121,9 @@ const editor = grapesjs.init({
             app_url + "/theme/css/custom.css",
             app_url + "/css/genchemph-drone.css",
             app_url + "/css/genchemph-design.css",
-            app_url + "/css/genchemph-about-us.css",
+            app_url + "/css/genchemph-about-us.css?v=3",
             app_url + "/css/genchemph-products.css?v=12",
-            app_url + "/css/genchemph-home-intro.css?v=5",
+            app_url + "/css/genchemph-home-intro.css?v=10",
             app_url + "/css/genchemph-contact-us.css",
             app_url + "/css/genchem-theme.css",
             app_url + "/css/genchemph-grapesjs.css?v=3",
@@ -1479,6 +1479,42 @@ function patchGenchemCanvasImages(doc) {
     });
 }
 
+function initGenchemTrustBar(doc) {
+    const root = doc || document;
+    const trustBar = root.querySelector(".footer-stick.dark .row.gy-4");
+    if (!trustBar) return;
+
+    const items = [
+        {
+            label: "Trusted Since 1976",
+            icon: '<svg class="me-3 genchem-trust-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="1.25em" height="1.25em" fill="currentColor" aria-hidden="true"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.1 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 2.1L288 439.1l-136.8 72.5c-8 4.2-17.9 3.8-25.3-2.1s-11.2-14.5-9.7-23.5l26.2-155.6L31.4 218c-6.4-6.4-8.6-15.9-5.9-24.5s10.3-15 19.3-16.3l153.2-22.6L266.3 13.5c4-8.3 12.4-13.5 21.6-13.5z"/></svg>',
+        },
+        {
+            label: "Quality Assured Products",
+            icon: '<svg class="me-3 genchem-trust-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1.25em" height="1.25em" fill="currentColor" aria-hidden="true"><path d="M256 0c4.6 0 9.2 1 13.4 2.9L457.7 82.8c22 9.3 38.4 31 38.3 57.2-.5 99.2-41.3 280.7-213.6 363.2-16.7 8-36.1 8-52.8 0-172.3-82.6-213.1-264.1-213.6-363.2-.1-26.2 16.3-47.9 38.3-57.2L242.7 2.9C246.8 1 251.4 0 256 0zm0 66.8V444.8C394 378 431.1 230.1 432 141.4L256 66.8l0 0z"/></svg>',
+        },
+        {
+            label: "24hr Technical Support",
+            icon: '<svg class="me-3 genchem-trust-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.25em" height="1.25em" fill="currentColor" aria-hidden="true"><path d="M12 2C6.48 2 2 6.03 2 11v4c0 1.1.9 2 2 2h1v-6H4c0-4.42 3.58-8 8-8s8 3.58 8 8h-1v6h1c1.1 0 2-.9 2-2v-4c0-4.97-4.48-9-10-9z"/><path d="M5 14h2v6H5c-1.1 0-2-.9-2-2v-2c0-1.1.9-2 2-2zm14 0c1.1 0 2 .9 2 2v2c0 1.1-.9 2-2 2h-2v-6h2z"/></svg>',
+        },
+    ];
+
+    trustBar.querySelectorAll(".col-lg-4").forEach((col, index) => {
+        const labelEl = col.querySelector("span");
+        const label = (labelEl && labelEl.textContent ? labelEl.textContent.trim() : "") || (items[index] && items[index].label) || "";
+        const item = items.find((entry) => entry.label === label) || items[index];
+        if (!item) return;
+
+        col.querySelectorAll("img, i, svg.genchem-trust-icon").forEach((node) => node.remove());
+
+        if (labelEl) {
+            labelEl.insertAdjacentHTML("beforebegin", item.icon);
+        } else {
+            col.insertAdjacentHTML("afterbegin", item.icon);
+        }
+    });
+}
+
 function setupGenchemCanvas() {
     const doc = editor.Canvas.getDocument();
     if (!doc || !doc.body) return;
@@ -1494,17 +1530,20 @@ function setupGenchemCanvas() {
     patchAllGenchemEditorComponents();
     initGenchemProductCards(doc);
     initGenchemProductTabs(doc);
+    initGenchemTrustBar(doc);
     window.setTimeout(() => {
         patchGenchemCanvasImages(doc);
         patchAllGenchemEditorComponents();
         initGenchemProductCards(doc);
         initGenchemProductTabs(doc);
+        initGenchemTrustBar(doc);
     }, 50);
     window.setTimeout(() => {
         patchGenchemCanvasImages(doc);
         patchAllGenchemEditorComponents();
         initGenchemProductCards(doc);
         initGenchemProductTabs(doc);
+        initGenchemTrustBar(doc);
     }, 300);
 
     doc.querySelectorAll(".video-wrap video").forEach((video) => {
